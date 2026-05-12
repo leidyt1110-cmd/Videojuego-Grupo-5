cc7b3b1c9085c39a0baf40e780f7a956c6cfdd39
ju4ntores <tjuan7638@gmail.com>
2026-05-12 14:52:53 -0500

    Update ControlEntradaSalida.cs

--- Juego/Prototipo funcional/Códigos/ControlEntradaSalida.cs
+++ Juego/Prototipo funcional/Códigos/ControlEntradaSalida.cs
@@ -28,4 +28,16 @@ public class ControlEntradaSalida : MonoBehaviour {
         }
     }
 }
-// 
+// Implementation of Game State Triggers: I developed the ControlEntradaSalida script to manage the logical transitions within the game levels.
+This script uses Unity’s OnTriggerEnter and OnTriggerExit methods to detect when an object tagged as "Player" interacts with specific boundaries.
+Event Notification System: I integrated a console-based feedback system using Debug.
+Log to signal key game events, such as the "START" message 
+upon entering a zone and the "VICTORY" message when a player successfully clears or exits a designated area.
+ Collision Logic and Optimization: I implemented tag-based filtering (other.CompareTag("Player"))
+ to ensure that environment triggers are only activated by the user,
+ preventing bugs caused by other physics objects or enemies interacting with the logic.
+System Scalability: The script was designed with public variables
+ (mensajeEntrada, mensajeSalida), 
+allowing other team members to easily customize zone messages or trigger events like opening doors
+, toggling lights, or starting background music directly from the Unity Inspector.//
+
