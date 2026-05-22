f2caec629b4d4eeb5af2fd5ce57496060f0d63fa
leidyt1110-cmd <leidyt1110@gmail.com>
2026-05-22 00:23:01 -0500

    Nota
    El que no se implemento para código de texto fue el script ControlEntradaSalida debido a que se realizo un nuevo script para victoria

---
+++ Juego/Prototipo funcional/Códigos/Códigos Finales/CameraFollow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Personaje que seguirá la cámara
+    public Transform jugador;
+
+    // Distancia entre cámara y personaje
+    public float offsetX = 3f;
+
+    // Suavidad del movimiento
+    public float suavizado = 5f;
+
+    // Posición fija en Y y Z
+    private float posicionY;
+    private float posicionZ;
+
+    void Start()
+    {
+        // Guardamos la posición inicial de la cámara
+        posicionY = transform.position.y;
+        posicionZ = transform.position.z;
+    }
+
+    void LateUpdate()
+    {
+        float nuevaX = Mathf.Max(transform.position.x, jugador.position.x + offsetX);
+
+        // Solo seguimos el eje X
+        Vector3 objetivo = new Vector3(
+            jugador.position.x + offsetX,
+            posicionY,
+            posicionZ
+        );
+
+        // Movimiento suave
+        transform.position = Vector3.Lerp(
+            transform.position,
+            objetivo,
+            suavizado * Time.deltaTime
+        );
+    }
+}
---
+++ Juego/Prototipo funcional/Códigos/Códigos Finales/Controladorvictoria.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Controladorvictoria : MonoBehaviour
+{
+    // Panel victoria
+    public GameObject panelVictoria;
+
+    // Texto monedas
+    public Text textoMonedasFinal;
+
+    // Referencia al jugador
+    private PlayerMovement jugador;
+
+    private void Start()
+    {
+        // Apagar panel al iniciar
+        if (panelVictoria != null)
+        {
+            panelVictoria.SetActive(false);
+        }
+    }
+
+    // Cuando el jugador llega a la meta
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Obtener el script PlayerMovement
+            jugador = other.GetComponent<PlayerMovement>();
+
+            // Activar victoria
+            ActivarVictoria();
+        }
+    }
+
+    void ActivarVictoria()
+    {
+        // Mostrar panel
+        panelVictoria.SetActive(true);
+
+        // Mostrar monedas reales
+        if (textoMonedasFinal != null && jugador != null)
+        {
+            textoMonedasFinal.text =
+                "Monedas: " + jugador.monedasRecolectadas;
+        }
+
+        // Pausar juego
+        Time.timeScale = 0f;
+
+        // Mostrar cursor
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    //Botón reiniciar juego
+    public void ReiniciarJuego()
+    {
+        Time.timeScale = 1f;
+        DynamicGI.UpdateEnvironment();
+
+        SceneManager.LoadScene(
+            SceneManager.GetActiveScene().buildIndex
+        );
+    }
+
+    // Botón salir
+    public void SalirdelJuego()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
+    }
+}
